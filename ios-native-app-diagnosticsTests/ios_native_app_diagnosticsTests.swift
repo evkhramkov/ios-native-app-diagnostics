b4b0c547b6f787f984d262a4c434fb209526d30c
Evgenii Khramkov <evkhramk@microsoft.com>
2018-09-13 20:08:23 +0300

    Initial Commit

---
+++ ios-native-app-diagnosticsTests/ios_native_app_diagnosticsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_native_app_diagnosticsTests.swift
+//  ios-native-app-diagnosticsTests
+//
+//  Created by Evgenii Khramkov on 9/13/18.
+//  Copyright © 2018 App Center. All rights reserved.
+//
+
+import XCTest
+@testable import ios_native_app_diagnostics
+
+class ios_native_app_diagnosticsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
